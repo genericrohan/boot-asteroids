dba35ee1586eaa089dc5a3fb8b546c66f9e86117
genericrohan <rohan@genericfruit.com>
2024-09-18 00:18:06 -0400

    failing to render triangle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,13 +9,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        player.draw(screen)
     
 if __name__ == "__main__":
     main()
